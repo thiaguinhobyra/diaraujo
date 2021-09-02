008f379801f06d79df0398f04b8cf302d5fd427c
thiaguinhobyra <85735157+thiaguinhobyra@users.noreply.github.com>
2021-09-02 11:45:32 -0300

    Add files via upload

---
+++ concatenacao.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Curso PHP</title>
+</head>
+<body>
+	<?php
+		//string
+		$nome = 'jorge sant ana';
+		//int
+		$idade = 35;
+		//float
+		$peso = 75.4;
+		//boolean
+		$fumante = true; //(true = 1) ou (false = vazio)
+		// logica
+		$idade = 36;
+		// operador .
+		echo 'ola ' . $nome . ', idade ' . $idade . ', peso ' . $peso . ' fumante ' . $fumante;
+		// aspas duplas
+		echo '<br />';
+
+		echo "Ola $nome"
+	?>
+</body>
+</html>
